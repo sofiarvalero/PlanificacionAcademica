0ad1fd39de2db75085a7475a47771c98777d4334
juanSteven18 <159735844+juanSteven18@users.noreply.github.com>
2024-02-11 23:12:02 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
 
+const ProfesoresRoute = require('./profesores.route');
+const MateriasRoute = require('./materias.route');
+const SeccionesRoute = require('./secciones.route');
+const EventosRoute = require('./eventos.route');
+
+
+router.use('/profesores', ProfesoresRoute);
+router.use('/materias', MateriasRoute);
+router.use('/secciones', SeccionesRoute);
+router.use('/eventos', EventosRoute);
+
+
+module.exports = router;
